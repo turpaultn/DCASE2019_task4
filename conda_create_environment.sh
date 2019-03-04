edc43e1df353bef6c09c147491aff9d61b7fd56b
turpault <turpaultn@gmail.com>
2019-03-05 00:04:02 +0100

    conda environment

---
+++ conda_create_environment.sh
@@ -0,0 +1,9 @@
+conda create -y -n dcase2019 python=3.6
+conda install -y pytorch torchvision -c pytorch # for gpu install (or cpu in MAC)
+# conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
+conda install -y pandas h5py scipy
+conda install -y pysoundfile librosa youtube-dl tqdm -c conda-forge
+conda install -y ffmpeg -c conda-forge
+
+pip install dcase_util
+pip install sed-eval
