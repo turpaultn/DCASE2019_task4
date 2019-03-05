35b76e7d86d9fe6663997aee3b1f64a202ecc389
wld41 <wld41@vm-pul51.explor>
2019-03-05 08:53:04 +0100

    fix conda  env creation

--- conda_create_environment.sh
+++ conda_create_environment.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 conda create -y -n dcase2019 python=3.6
+source activate dcase2019
+conda install -y pandas h5py scipy
 conda install -y pytorch torchvision -c pytorch # for gpu install (or cpu in MAC)
 # conda install pytorch-cpu torchvision-cpu -c pytorch (cpu linux)
-conda install -y pandas h5py scipy
 conda install -y pysoundfile librosa youtube-dl tqdm -c conda-forge
 conda install -y ffmpeg -c conda-forge
 
